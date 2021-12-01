0fee1b80ed9bdff346fa573e4daf764546ec3629
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2021-12-01 08:21:25 -0300

    Pagina de usuarios responsiva

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,70 +12,81 @@ import {
   Tbody,
   Checkbox,
   Text,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
-import { SideBar } from "../../components/SideBar";
+import { Sidebar } from "../../components/Sidebar";
+import Link from "next/link";
 
 export default function UsersList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Box>
       <Header />
 
       <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
-        <SideBar />
+        <Sidebar />
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justifyContent="space-between" align="center">
             <Heading fontSize="lg" fontWeight="normal">
               Usuários
             </Heading>
 
-            <Button
-              as="a"
-              size="sm"
-              fontSize="sm"
-              colorScheme="pink"
-              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-              cursor="pointer"
-            >
-              Criar novo
-            </Button>
+            <Link href="/users/create" passHref>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+                cursor="pointer"
+              >
+                Criar novo
+              </Button>
+            </Link>
           </Flex>
 
           <Table colorScheme="whiteAlpha">
             <Thead>
-              <Tr px="6" color="gray.300" w="8">
-                <Th>
+              <Tr>
+                <Th px={["4", "4", "6"]} color="gray.300" w="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuários</Th>
-                <Th>Data de cadastro</Th>
+                {isWideVersion && <Th>Data de cadastro</Th>}
                 <Th w="8"></Th>
               </Tr>
             </Thead>
 
             <Tbody>
               <Tr>
-                <Td>
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
                   <Text>Tarcisio Delmondes</Text>
                   <Text>tarcisiodelmondes@gmail.com</Text>
                 </Td>
-                <Td>05 de agosto, 2021</Td>
+                {isWideVersion && <Td>05 de agosto, 2021</Td>}
                 <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="teal"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    cursor="pointer"
-                  >
-                    Editar
-                  </Button>
+                  {isWideVersion && (
+                    <Button
+                      as="a"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="teal"
+                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                      cursor="pointer"
+                    >
+                      {isWideVersion ? "Editar" : ""}
+                    </Button>
+                  )}
                 </Td>
               </Tr>
             </Tbody>
