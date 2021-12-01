af026d17cb2893431253ffe2ed90d3ef94be90b8
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2021-12-01 08:24:30 -0300

    Configuraçao do contexto da SidebarDrawer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/dist/shared/lib/router/router";
+import { SidebarDrawerContextProvider } from "../context/SidebarDrawerContext";
 import { theme } from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <SidebarDrawerContextProvider>
+        <Component {...pageProps} />
+      </SidebarDrawerContextProvider>
     </ChakraProvider>
   );
 }
