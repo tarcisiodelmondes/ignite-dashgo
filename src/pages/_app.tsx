116996fdda3dd0b42d440f090fa3f282f956af45
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2022-01-14 11:10:12 -0300

    feat: creating a service queryClient

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/dist/shared/lib/router/router";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { SidebarDrawerContextProvider } from "../context/SidebarDrawerContext";
 import { makeServer } from "../services/mirage";
+import { queryClient } from "../services/queryClient";
 import { theme } from "../styles/theme";
 
 const isDev = process.env.NODE_ENV === "development";
@@ -11,8 +12,6 @@ const isDev = process.env.NODE_ENV === "development";
 if (isDev) {
   makeServer();
 }
-
-const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
