5aa4567c7bf5c1c4e33a5eaa029bd35558c356a2
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2022-01-13 22:45:52 -0300

    feat: config miragejs and react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,30 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/dist/shared/lib/router/router";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { SidebarDrawerContextProvider } from "../context/SidebarDrawerContext";
+import { makeServer } from "../services/mirage";
 import { theme } from "../styles/theme";
+
+const isDev = process.env.NODE_ENV === "development";
+
+if (isDev) {
+  makeServer();
+}
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerContextProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerContextProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerContextProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerContextProvider>
+      </ChakraProvider>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
